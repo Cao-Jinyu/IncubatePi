3ff5924b7f47f760e7cc00193cccc6248e8f519d
2077762b <2077762b@student.gla.ac.uk>
2018-03-02 16:18:29 +0000

    Updated PWMCtrl. Still having issue with toString so may not compile.

--- Software/PWMCtrl.cpp
+++ Software/PWMCtrl.cpp
@@ -1,216 +1,147 @@
 #include <iostream>
 #include <fstream>
-#include <unistd.h>
+#include <string>
+#include <stdio.h>     
 #include <stdlib.h>
 #include "PWMCtrl.h"   
 
 static std::string EXPORTFILE = "/sys/class/pwm/pwmchip0/export";
-static std::string PERIODFILE = "/sys/class/pwm/pwmchip0/pwm1/period";
-static std::string DUTYCYCLEFILE = "/sys/class/pwm/pwmchip0/pwm1/duty_cycle";
-static std::string ENABLEFILE = "/sys/class/pwm/pwmchip0/pwm1/enable";
+static std::string BASEFILE = "/sys/class/pwm/pwmchip0/pwm";
+static std::string PERIODFILE = "/period";
+static std::string DUTYCYCLEFILE = "/duty_cycle";
+static std::string ENABLEFILE = "/enable";
 
-static const std::string TEMPFILE = "temp";
-static std::string MODULECHECK;
+static std::string TEMPFILE = "temp";
+static std::string MODULECHECK = "lsmod | grep pwm > " + TEMPFILE;
 
 static const int MIN_PERIOD = 10000;        // Minimum allowed PWM period	
-static const int PWMPORT = 1;
 static const int ENABLE = 1;
 static const int DISABLE = 0;
+static const int NUM_OF_CHIPS = 2;
 
-int PWMCtrl::setupCheck;
-int PWMCtrl::configureCheck;
-int PWMCtrl::period;
-
-
-PWMCtrl::PWMCtrl() {
-    PWMCtrl::setupCheck = 0;
-    PWMCtrl::configureCheck = 0;
-    PWMCtrl::period = 0;
-}
-
-/*
-    Calls the neccessary functions to set up the PWM channel.
-    Returns 0 and sets setupCheck on success.
-    Returns 1 on failure.
-*/
-int PWMCtrl::setup() {
-    MODULECHECK = "lsmod | grep pwm > " + TEMPFILE;
-
-    if (moduleCheck()) return 1;
-    if (exportPWM()) return 1;
-    PWMCtrl::setupCheck = 1;
-    return 0;
-}
-
-/*
-    Calls the neccessary functions to configure the PWM channel.
-    Returns 0 and sets configureCheck on success.
-    Returns 1 on failure.
-*/
-int PWMCtrl::configure(int period, int dutyCycle) {
-    if (!PWMCtrl::setupCheck) {
-        std::cerr << "Couldn't configure PWM channel as setup function has not been performed" << std::endl;
-        return 1;
-    }
-    if (setPeriod(PWMCtrl::period)) return 1;
-    if (setDutyCycle(dutyCycle)) return 1;
-    PWMCtrl::configureCheck = 1;
-    std::cout << "Period: " << PWMCtrl::period << ", Duty Cycle: " << dutyCycle << std::endl;
-    return 0;
-}
-
-/*
-	Checks that the PWM module is loaded in the linux kernel.
-	Returns 0 on success.
-	Returns 1 on failure.
-*/
-int PWMCtrl::moduleCheck(){
+PWMCtrl::PWMCtrl(int pwmchip){
 		
-	std::ifstream infile;
+	std::ifstream tempFile;
+	std::ofstream exportFile;
 	std::string line;
+
+	// Check the pwm chip number is a valid for the RPI
+	if (pwmchip < 0 || pwmchip >= NUM_OF_CHIPS){
+		std::cerr << "Couldn't create PWMCtrl as chip number is invalid" << std::endl;
+		throw new std::exception();
+	}
 	
     // Execute Linux command that writes any modules containing the string pwm to a file temp.
 	if (system(MODULECHECK.c_str()) == -1) {
 		std::cerr << "PWM Module Check Error: Couldn't execute lsmod linux system command" << std::endl; 
-		return 1;
+		throw new std::runtime_error(NULL);
 	}
 	
 	// Opens the file where the output of the command was piped to.
-	infile.open(TEMPFILE.c_str());
-	if (!infile.is_open()) {
+	tempFile.open(TEMPFILE.c_str());
+	if (!tempFile.is_open()) {
 		std::cerr << "PWM Module Check Error: Couldn't open temp file" << std::endl;
-		return 1; 
+		throw new std::runtime_error(NULL);
 	}	
 	
-	// Checks that the file is not empty and prints out the module name. 
-	if (!getline(infile,line)) {
-		printf("Couldn't find the neccessary kernal modules\n");
-		infile.close();
-  		return 1;
-	} else std::cout << "Module: " << line << std::endl;	 	
-	infile.close();
-	return 0;
-}
+	// Checks that the file is not empty. 
+	if (!getline(tempFile,line)) {
+		printf("PWM Module Check Error: Couldn't find the neccessary kernal modules\n");
+		tempFile.close();
+		throw new std::runtime_error(NULL);
+	}		 	
+	tempFile.close();
 
-/*
-    Exports the PWM channel.
-	Returns 0 on success.
-	Returns 1 on failure.
-*/
-int PWMCtrl::exportPWM() {
-
-    std::ofstream outfile;
-	
-	// Open the linux PWM export file
-	outfile.open(EXPORTFILE.c_str());
-	if(!outfile.is_open()) {
+	// Open the linux PWM export file.
+	exportFile.open(EXPORTFILE.c_str());
+	if(!exportFile.is_open()) {
 		std::cerr << "Couldn't carry out export as couldn't open PWM export linux system file" << std::endl;
-		return 1;   
+		throw new std::runtime_error(NULL);   
 	}
 
-	// Write out export
-	outfile << PWMPORT << std::endl;
-	outfile.close();
-	return 0;
+	// Write out the pwmchip number to export that chip.
+	exportFile << pwmchip << std::endl;
+	exportFile.close();
+
+	// Create pwm linux system file names.
+	periodFileName = BASEFILE + std::toString(pwmchip) + PERIODFILE;
+    dutyCycleFileName = BASEFILE + std::toString(pwmchip) + DUTYCYCLEFILE;
+    enableFileName = BASEFILE + std::toString(pwmchip) + ENABLEFILE;
+
 }
 
-/*
-    Sets the PWM period by writing to the neccesary linux system file.
-    Returns 0 on success.
-    Returns 1 on failure.
-*/
-int PWMCtrl::setPeriod(int set_period) {
+void PWMCtrl::configure(int period, int dutyCycle) {
 	
-	std::ofstream outfile;
+	std::ofstream periodFile;
+	std::ofstream dutyCycleFile;
 	
-	// Check that the new period is valid.
-	if (set_period < MIN_PERIOD) {
-		std::cerr << "Invalid period. Must be greater than" << MIN_PERIOD << "." << std::endl;
-		return 1;
+	// Check that the period is valid.
+	if (period < MIN_PERIOD) {
+		std::cerr << "Invalid period. Must be greater than " << MIN_PERIOD << "." << std::endl;
+		throw new std::exception();
+	}
+
+	// Check that the duty cycle is valid
+	if (dutyCycle < 0 || dutyCycle > period) {
+		std::cerr << "Invalid duty cycle. Must be between 0 and the period." << std::endl;
+		throw new std::exception();
 	}
 	
 	// Open the linux PWM period file.
-	outfile.open(PERIODFILE.c_str());
-	if(!outfile.is_open()) {
+	periodFile.open(periodFileName.c_str());
+	if(!periodFile.is_open()) {
 		std::cerr << "Couldn't update period as couldn't open PWM period linux system file" << std::endl;
-		return 1;   
+		throw new std::runtime_error(NULL); 
 	}
 
-	// Write out period
-	outfile << set_period << std::endl;
-	PWMCtrl::period = set_period;
-	outfile.close();
-	return 0;
-}
+	// Write out period to linux PWM period file
+	periodFile << period << std::endl;
+	periodFile.close();
 
-/*
-    Sets the PWM duty cycle by writing to the neccesary linux system file
-    Returns 0 on success
-    Returns 1 on failure
-*/
-int PWMCtrl::setDutyCycle(int dutyCycle) {
-	
-	std::ofstream outfile;
-	
-	// Check that the duty cycle is valid
-	if (dutyCycle < 0 || dutyCycle > PWMCtrl::period) {
-		std::cerr << "Invalid duty cycle. Must be between 0 and the period." << std::endl;
-		return 1;
-	}
-	
 	// Open the linux PWM duty cycle file
-	outfile.open(DUTYCYCLEFILE.c_str());
-	if(!outfile.is_open()) {
+	dutyCycleFile.open(dutyCycleFileName.c_str());
+	if(!dutyCycleFile.is_open()) {
 		std::cerr << "Couldn't update duty cycle as couldn't open PWM duty cycle linux system file" << std::endl;
-		return 1;   
+		throw new std::runtime_error(NULL);  
 	}
 
-	// Write out duty cycle 
-	outfile << dutyCycle << std::endl;
-	outfile.close();
-	return 0;
+	// Write out duty cycle to linux PWM duty cycle file
+	dutyCycleFile << dutyCycle << std::endl;
+	dutyCycleFile.close();
+
+	std::cout << "Period: " << period << ", Duty Cycle: " << dutyCycle << std::endl;
+
 }
 
-/*
-    Enables PWM by writing to the neccesary linux system file
-    Returns 0 on success
-    Returns 1 on failure
-*/
-int PWMCtrl::enable() {
+void PWMCtrl::enable(){
 
-	std::ofstream outfile;
+	std::ofstream enableFile;
 	
 	// Open the linux PWM enable file
-	outfile.open(ENABLEFILE.c_str());
-	if(!outfile.is_open()) {
+	enableFile.open(enableFileName.c_str());
+	if(!enableFile.is_open()) {
 		std::cerr << "Couldn't enable PWM as couldn't open PWM enable linux system file" << std::endl;
-		return 1;   
+		throw new std::runtime_error(NULL);  
 	}
 
 	// Write out enable
-	outfile << ENABLE << std::endl;
-	outfile.close();
-	return 0;
+	enableFile << ENABLE << std::endl;
+	enableFile.close();
 }
 
-/*
-    Disables PWM by writing to the neccesary linux system file
-    Returns 0 on success
-    Returns 1 on failure
-*/
-int PWMCtrl::disable()
-{	
-	std::ofstream outfile;
+void PWMCtrl::disable(){	
+
+	std::ofstream enableFile;
 	
 	// Open the linux PWM enable file
-	outfile.open(ENABLEFILE.c_str());
-	if(!outfile.is_open()) {
+	enableFile.open(enableFileName.c_str());
+	if(!enableFile.is_open()) {
 		std::cerr << "Couldn't disable PWM as couldn't open PWM enable linux system file" << std::endl;
-		return 1;   
+		throw new std::runtime_error(NULL);     
 	}
 
 	// Write out disable
-	outfile << DISABLE << std::endl;
-	outfile.close();
-	return 0;
+	enableFile << DISABLE << std::endl;
+	enableFile.close();
+	
 }
--- Software/PWMCtrl.h
+++ Software/PWMCtrl.h
@@ -2,25 +2,45 @@
 #define PWM_CTRL
 
 /*
-    This class is used to control the PWM1 chip on the RPI
+    This class is used to control a PWM chip on a Raspberry Pi
 */
 class PWMCtrl {
-    private:
-        static int setupCheck;
-        static int configureCheck;
-        static int period;
 
-        static int moduleCheck();
-        static int exportPWM();
-        static int setPeriod(int period);
-        static int setDutyCycle(int dutyCycle);
+    private:
+
+        std::string periodFileName;         // Generated linux system PWM period file name based on chip no.
+        std::string dutyCycleFileName;      // Generated linux system PWM duty cycle file name based on chip no.
+        std::string enableFileName;         // Generated linux system PWM enable file name based on chip no.
 
     public:
-        PWMCtrl();
-        static int setup();
-        static int configure(int period, int dutyCycle);
-        static int enable();
-        static int disable();
+
+        /*
+            Creates a new PWMctl for the specified pwmchip. 
+            Throws an exception if the chip number is invalid
+            Throws a runtime error if the pwm module check was not successful
+            or if the pwm module was not successfully exported
+        */
+        PWMCtrl(int pwmchip);
+
+        /* 
+            Sets the period and duty cycle of the pwm chip
+            Throws an exception if the period or duty cycle is invalid
+            (period < MIN_PERIOD or duty cycle > period).
+            Throws a runtime_error if the period or duty cycle was not written successfully.
+        */
+        void configure(int period, int dutyCycle);
+
+        /*
+            Enables the pwm chip.
+            Throws a runtime_error if the linux system PWM enable file could not be accessed.
+        */
+        void enable();
+
+        /*
+            Disables the pwm chip.
+            Throws a runtime_error if the linux system PWM enable file could not be accessed.
+        */
+        void disable();
 };
 
 #endif
