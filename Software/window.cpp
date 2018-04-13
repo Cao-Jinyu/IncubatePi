b7769cc0c814126cfaa637a53188697098b7625e
2077762b <2077762b@student.gla.ac.uk>
2018-04-13 11:22:02 +0000

    Fixed minor compiler bug for xDataSet

--- Software/window.cpp
+++ Software/window.cpp
@@ -8,34 +8,33 @@ Window::Window(PID *pid, TempReader *tempRead) : gain(40), count(0)
 {
     pidControl = pid;
     this->tempRead = tempRead;
-    
+
 	knob = new QwtKnob;
-	// set up the gain knob
+	// Set up the gain knob
 	knob->setValue(gain);
 
-	// use the Qt signals/slots framework to update the gain -
-	// every time the knob is moved, the setGain function will be called
+	// Use the Qt signals/slots framework to update the gain -
+	// Every time the knob is moved, the setGain function will be called
 	connect( knob, SIGNAL(valueChanged(double)), SLOT(setGain(double)) );
-    
 
 	curveSet = new QwtPlotCurve;
     curveCurrent = new QwtPlotCurve;
 	plot = new QwtPlot;
-	// make a plot curve from the data and attach it to the plot
+    
+	// Make a plot curve from the data and attach it to the plot
 	curveCurrent->setSamples(xDataCurrent, yDataCurrent, plotDataSize);
 	curveCurrent->attach(plot);
-    curveSet->setSamples(xdataSet, yDataSet, plotDataSize);
+    curveSet->setSamples(xDataSet, yDataSet, plotDataSize);
     curveSet->attach(plot);
 
 	plot->replot();
 	plot->show();
 
-
-	// set up the layout - knob above thermometer
+	// Set up the layout - knob above thermometer
 	vLayout = new QVBoxLayout;
 	vLayout->addWidget(knob);
 
-	// plot to the left of knob and thermometer
+	// Plot to the left of knob and thermometer
 	hLayout = new QHBoxLayout;
 	hLayout->addLayout(vLayout);
 	hLayout->addWidget(plot);
@@ -44,22 +43,22 @@ Window::Window(PID *pid, TempReader *tempRead) : gain(40), count(0)
 }
 
 Window::~Window() {
-	// tells the thread to no longer run its endless loop
-//	adcreader->quit();
-	// wait until the run method has terminated
-//	adcreader->wait();
-//	delete adcreader;
+	// Tells the thread to no longer run its endless loop
+    // adcreader->quit();
+	// Wait until the run method has terminated
+    // adcreader->wait();
+    // delete adcreader;
 }
 
 void Window::timerEvent( QTimerEvent * )
 {   
 
-    //These variables are the inputs to the graph. They need to be changed to be inputs from the PID controller
+    // These variables are the inputs to the graph. They need to be changed to be inputs from the PID controller
 	double currentTemp = tempRead->readTemp();
     double setTemp = gain;
 	++count;
 
-	// add the new input to the plot
+	// Add the new input to the plot
 	memmove( yDataCurrent, yDataCurrent+1, (plotDataSize-1) * sizeof(double) );
     memmove( yDataSet, yDataSet+1, (plotDataSize-1) * sizeof(double) );
 	yDataCurrent[plotDataSize-1] = currentTemp;
@@ -70,10 +69,10 @@ void Window::timerEvent( QTimerEvent * )
 }
 
 
-// this function can be used to change the gain of the A/D internal amplifier
+// This function can be used to change the gain of the A/D internal amplifier
 void Window::setGain(double gain)
 {
-	// for example purposes just change the amplitude of the generated input
+	// For example purposes, just change the amplitude of the generated input
 	this->gain = gain;
     pidControl->update_required_value((float)gain);
 }
